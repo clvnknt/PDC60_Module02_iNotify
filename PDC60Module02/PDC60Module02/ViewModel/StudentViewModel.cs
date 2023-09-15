8765430d0f7d18358c32770e2954279c0ac41f24
clvnknt <p.calvinkent@gmail.com>
2023-09-15 11:31:34 +0800

    first commit

---
+++ PDC60Module02/PDC60Module02/ViewModel/StudentViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using PDC60Module02.Model;
+using Xamarin.Forms;
+
+namespace PDC60Module02.ViewModel
+{
+    class StudentViewModel : INotifyPropertyChanged
+    {
+        public Student StudentSet { get; set; }
+        public Command SubmitEntry {  get; set; }
+
+        public string SubjectCodeEntry { get; set; }
+        public string SubjectCodeDisplay { get; set; }
+
+        public string SubjectTitleEntry { get; set; }
+        public string SubjectTitleDisplay { get; set; }
+
+        public string SubjectUnitEntry { get; set; }
+        public string SubjectUnitDisplay { get; set; }
+
+        public StudentViewModel()
+        {
+            SubmitEntry = new Command(() =>
+            {
+                StudentSet = new Student
+                {
+                    SubjectCode = SubjectCodeEntry,
+                };
+
+                SubjectCodeDisplay = SubjectCodeEntry;
+                SubjectTitleDisplay = SubjectTitleEntry;
+                SubjectUnitDisplay = SubjectUnitEntry;
+
+                onPropertyChanged(nameof(SubjectCodeDisplay));
+                onPropertyChanged(nameof(SubjectCodeEntry));
+
+                onPropertyChanged(nameof(SubjectTitleDisplay));
+                onPropertyChanged(nameof(SubjectTitleEntry));
+
+                onPropertyChanged(nameof(SubjectUnitDisplay));
+                onPropertyChanged(nameof(SubjectUnitEntry));
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void onPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
